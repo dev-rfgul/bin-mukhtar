db645e57d888eced22632ee35cd96f6a8e63092b
dev-rfgul <rfgul587@gmail.com>
2025-07-27 13:41:39 +0500

    refactor: remove unnecessary whitespace in BlogsPage component for improved readability

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -8,12 +8,10 @@ const BlogsPage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-purple-900">
       <Navbar />
-
       {/* Enhanced Blogs Section */}
       <div className="pt-20"> {/* Add padding top to account for fixed navbar */}
         <Blogs />
       </div>
-
       <Footer />
     </div>
   );
