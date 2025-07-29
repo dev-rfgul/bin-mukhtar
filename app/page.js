268cc7fb4cb854843d344f070cf9be0b88c8d40c
dev-rfgul <rfgul587@gmail.com>
2025-07-29 18:02:15 +0500

    cleanup the page.js

--- app/page.js
+++ app/page.js
@@ -11,9 +11,7 @@ import Banner from "./Components/Banner";
 import Team from "./Components/Team";
 import FixedLogos from "./Components/FixedLogo";
 import Footer from "./Components/Footer";
-import Contact from "./Contact/Contact";
 import Blogs from "./Components/Blogs";
-// import BlogPost from "./1stBlog/BlogPost";
 import ScrollServices from "./Components/ScrollServices";
 
 const page = () => {
@@ -21,7 +19,6 @@ const page = () => {
   return (
     <div className="bg-gradient-to-br from-slate-900 via-blue-900 to-slate-800">
       <Navbar />
-      {/* <Animation /> */}
       <Banner
         heading={"Welcome to Bin Mukhtar and Co"}
         secondaryHeading={"Leading Corporate Consultant  in Pakistan"}
@@ -31,33 +28,9 @@ const page = () => {
         paragraph={""}
         buttonText="Contact Us  "
         imagePosition={"right"}
-        imgSrc="/images/primary-pic-new (1).png"
-      // orange pic is finalezee
-      />
+        imgSrc="/images/primary-pic-new (1).png"/>
       <ScrollServices />
       <Blogs />
-      {/* services component */}
-      {/* <Testimonials buttonText="View all services " /> */}
-      {/* <Awards
-        imgHeight={"w-full"}
-        heading=" Featured Blogs"
-        buttonText={"Read More"}
-        bacol=" section bg-secondary"
-        imgSrcArray={[
-          "./images/blog-2.jpg",
-          "./images/blog-9.jpg",
-          "./images/blog-1.jpg",
-          "./images/blog-5.jpg",
-        ]}
-        headingArray={[
-          "FBR’s New Directive: A Step Towards Transparent Taxation in Pakistan",
-          "How Freelancers Can File Their Income Tax Returns?",
-          "Are You Required to File a Tax Return in Pakistan?",
-          "A Guide on How to Run a Leather Export Busine",
-        ]}
-        textArray={["", "", "", ""]}
-        routePath={["/1stBlog", "/2ndBlog", "/3rdBlog", "/4thBlog"]}
-      /> */}
       <Collaborator />
       <Banner
         heading="Say hello to the app that made everything even more convenient"
