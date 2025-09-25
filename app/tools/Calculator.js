a0050c9b795d35cff55b17c07cb776d3c8346943
dev-rfgul <rfgul587@gmail.com>
2025-09-25 10:41:49 +0500

    changed the text of the input field of salary

--- app/tools/Calculator.js
+++ app/tools/Calculator.js
@@ -75,12 +75,12 @@ function Calculator() {
           </div>
 
           <div className="w-full">
-            <label className="block text-gray-200 text-sm font-medium mb-2" htmlFor="monthlySalary">
+            <label className="block text-black-200 text-sm font-medium mb-2" htmlFor="monthlySalary">
               Enter Monthly Salary
             </label>
             <input
               id="monthlySalary"
-              className="w-full rounded-md border border-white/10 bg-white/3 py-2 px-3 text-white placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-emerald-300"
+              className="w-full rounded-md border border-white/10 bg-white/3 py-2 px-3 text-black placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-emerald-300"
               type="text"
               value={monthlySalary}
               onChange={handleMonthlySalaryChange}
